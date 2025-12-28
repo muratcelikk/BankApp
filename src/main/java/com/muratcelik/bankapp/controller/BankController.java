19cca7f04e8d3f127ac6e2f9ea4ffe3c392a2af2
muratcelikk <muratcelik11@gmail.com>
2025-12-29 02:51:21 +0300

    251229_02:50_bankapp_2

--- src/main/java/com/muratcelik/bankapp/controller/BankController.java
+++ src/main/java/com/muratcelik/bankapp/controller/BankController.java
@@ -1,13 +1,14 @@
 package com.muratcelik.bankapp.controller;
 
-import com.muratcelik.bankapp.model.Account;
 import com.muratcelik.bankapp.repository.AccountRepository;
 import com.muratcelik.bankapp.service.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/bank")
@@ -27,10 +28,5 @@ public class BankController {
         } catch (Exception e) {
             return "Hata: " + e.getMessage();
         }
-    }
-
-    @GetMapping("/accounts")
-    public List<Account> getAllAccounts() {
-        return accountRepository.findAll();
     }
 }
